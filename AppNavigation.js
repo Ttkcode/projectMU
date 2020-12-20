b212ae3e390babd2f008a7a79850a8f64bb34a6f
Ttkcode <74504483+Ttkcode@users.noreply.github.com>
2020-12-20 18:47:26 +0300

    Update AppNavigation.js

--- AppNavigation.js
+++ AppNavigation.js
@@ -1,1 +1,11 @@
 import { createAppContainer } from 'react-navigation';
+function MainNavigator() {
+  return(
+    <Stack.Navigator
+      screenOptions={{
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            textAlign: 'center',
+            alignSelf: 'center',
+            flex: 1,
+          }
