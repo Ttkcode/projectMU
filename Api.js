7c1fec2e6bef7ff1d08d31f42ab6d759088293b5
Ttkcode <74504483+Ttkcode@users.noreply.github.com>
2020-12-21 21:05:22 +0300

    Update Api.js

--- Api.js
+++ Api.js
@@ -1,8 +1,19 @@
-C#
 var url = 'http://newsapi.org/v2/top-headlines?' +
           'country=us&' +
           'apiKey=53fca4c474624e8aa34a72625688e1fa';
 var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
+var url = 'http://newsapi.org/v2/everything?' +
+          'q=Apple&' +
+          'from=2020-12-21&' +
+          'sortBy=popularity&' +
+          'apiKey=53fca4c474624e8aa34a72625688e1fa';
+
+var req = new Request(url);
+
 fetch(req)
     .then(function(response) {
         console.log(response.json());
